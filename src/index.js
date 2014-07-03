63aa412194a2ab5e2c1fcaa518c29978991fe8a7
Muhammad Safraz Razik <mr.safraz@gmail.com>
2014-07-03 12:09:52 +0530

    export Application class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import ko from 'knockout';
-import koPunches from 'durandal-punches';
-
-ko.punches.enableAll();
-
 export {Module, RootModule} from './Module';
 export {ModuleLoader} from './ModuleLoader';
 export {RouteBuilder} from './RouteBuilder';
+export {Application} from './Application';
